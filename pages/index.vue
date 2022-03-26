ad5113be2b57792e0419a27b740ebef7897e9148
Divine <dpkreativ@gmail.com>
2022-03-26 12:27:00 +0100

    update markup to handle styling and responsiveness

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,27 @@
 <template>
-  <main class="w-full max-w-4xl mx-auto">
+  <main class="w-full max-w-4xl mx-auto px-5">
     <nav class="py-5 text-center font-bold text-lg">Nuxt Open Graph Demo</nav>
-    <section>
+    <section class="overflow-hidden rounded-xl">
       <img
         src="https://res.cloudinary.com/dpkreativ/image/upload/v1648291772/nuxt-open-graph-demo/webANXPursuitOfHappynessA_azzusy.jpg"
         alt="The Pursuit of Happyness: cover image"
+        class="object-cover"
       />
     </section>
-    <section class="p-5">
-      <h1 class="font-bold text-2xl lg:text-4xl">The Pursuit of Happyness</h1>
+    <section class="py-5">
+      <div class="lg:flex justify-between items-center">
+        <h1 class="font-bold text-2xl lg:text-4xl my-1">
+          The Pursuit of Happyness
+        </h1>
+        <a href="https://www.imdb.com/title/tt0454921/">
+          <button class="bg-yellow-400 font-black text-xl p-1 rounded shadow">
+            <p class="animate-pulse">IMDb</p>
+          </button>
+        </a>
+      </div>
       <div class="mt-5">
         <h2 class="font-semibold text-lg">Film Synopsis</h2>
-        <p>
+        <p class="mt-2">
           Life is a struggle for single father Chris Gardner (Will Smith).
           Evicted from their apartment, he and his young son (Jaden Christopher
           Syre Smith) find themselves alone with no place to go. Even though
